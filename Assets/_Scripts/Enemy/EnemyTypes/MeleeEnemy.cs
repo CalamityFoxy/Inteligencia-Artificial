d510fd1f102e9e3d1071ed51790458251a129ded
CalamityFoxy <134015131+CalamityFoxy@users.noreply.github.com>
2026-04-27 23:49:57 -0300

    Algunos comments(hay que comentar todo el codigo)

--- Assets/_Scripts/Enemy/EnemyTypes/MeleeEnemy.cs
+++ Assets/_Scripts/Enemy/EnemyTypes/MeleeEnemy.cs
@@ -36,13 +36,15 @@ public class MeleeEnemy : EnemyController
         _idleState = new EnemyIdleState(this, idleDuration);// estos los creo para que el behaviour Tree los guarde de referencia y los cambie despues y asi la FSM no se entera de lo que esta pasando dentro del estado ni sus metodos(ni deberia).
         _patrolState = new EnemyMelee_PatrolState(this, patrolWaypoints, iterationsBeforeRest);// lo mismo acá
 
+
+        //registramos los estados en la fsm
         meleeEnemyFsm.RegisterState(EnemyStateType.Idle, _idleState);
         meleeEnemyFsm.RegisterState(EnemyStateType.Patroll, _patrolState);
         meleeEnemyFsm.RegisterState(EnemyStateType.Flee, new EnemyFleeState(this, healingPoint));
         meleeEnemyFsm.RegisterState(EnemyStateType.Heal, new EnemyHealState(this));
         meleeEnemyFsm.RegisterState(EnemyStateType.Chase, new EnemyMelee_ChaseState(this, Target, attackRange, attackCooldown));
 
-
+        // creamos los nodos que vamos a utilizar en el BT
         ActionNode respawning = new ActionNode(Respawn);
         ActionNode searchFlag = new ActionNode(SearchFlag);
         ActionNode attackPlayer = new ActionNode(AttackPlayer);
@@ -62,7 +64,7 @@ public class MeleeEnemy : EnemyController
             meleeEnemyFsm.SetState(EnemyStateType.Chase);
         });
 
-        
+        // creamos el recorrido del BT, de raiz al ultimo
         QuestionNode idleOrPatrol = new QuestionNode(IdleFinished, patrol, idle);
         QuestionNode notSeeingPlayer = new QuestionNode(PatrolNeedsRest, idleOrPatrol, patrol);
         QuestionNode canSee = new QuestionNode(IsTargetTracked, chaseAfterRoll, notSeeingPlayer);
@@ -70,12 +72,14 @@ public class MeleeEnemy : EnemyController
         QuestionNode reachedHealZone = new QuestionNode(() => IsAtHealingZone(healingPoint), isHealed, flee);
         QuestionNode shouldFlee = new QuestionNode(ShouldFleeForHealing, reachedHealZone, canSee);
 
- 
+
         QuestionNode isAlive = new QuestionNode(IsAlive, shouldFlee, respawning);
+
+        
 
         rootNode = isAlive;
 
-        meleeEnemyFsm.SetInitialState(EnemyStateType.Patroll);
+        meleeEnemyFsm.SetInitialState(EnemyStateType.Patroll); // seteamos el estado default a patrullar
 
         
         
@@ -85,18 +89,24 @@ public class MeleeEnemy : EnemyController
 
     protected override void Update()
     {
-        base.Update();
+        base.Update(); 
         rootNode.Execute();
         meleeEnemyFsm.Execute();
+
+        // hacemos esto en el update para que cuando lo pierda al player, pueda volver a dar roll selection(decida si va ser berseker,normal o coward) en la proxima vez que lo vea.
         if (!IsTargetTracked())
-        {
-            _hasRolledReaction = false;
+        { 
+            _hasRolledReaction = false;   
+
         }
 
         Debug.Log(meleeEnemyFsm.CurrentState);
     }
     public bool IdleFinished() => _idleState != null && _idleState.IdleFinished;
     public bool PatrolNeedsRest() => _patrolState != null && _patrolState.ShouldRest;
+
+
+    // función  que determina el % de que salgan cada uno en Wheel Roulette.
     private CombatReaction RollCombatReaction()
     {
         var weights = new Dictionary<CombatReaction, float>
@@ -108,14 +118,13 @@ public class MeleeEnemy : EnemyController
 
         return Extensions.RouletteWheelSelection(weights);
     }
-
+    //la ejecutamos 
     private void TryRollReaction()
     {
         if (!_hasRolledReaction)
         {
             _currentReaction = RollCombatReaction();
             _hasRolledReaction = true;
-            Debug.Log($"Roulette result: {_currentReaction} | {gameObject.name}");
 
         }
     }
@@ -126,15 +135,16 @@ public class MeleeEnemy : EnemyController
 
         float threshold;
 
+        // cantidad de vida para que vuelvan, dependiendo la cantidad de treshold entre 0 (sin vida) o 1f (full vida). Como habiamos dicho normal se vuelve al 25% a curarse, el coward en 1/3 y berseker pelea hasta la muerte
         switch (_currentReaction)
         {
             case CombatReaction.Berserker:
                 return false;
             case CombatReaction.Normal:
-                threshold = 0.25f;
+                threshold = 0.25f; 
                 break;
             case CombatReaction.Coward:
-                threshold = 0.66f;
+                threshold = 0.66f; // cantidad de vida para que vuelvan 1/3
                 break;
             default:
                 return false;
