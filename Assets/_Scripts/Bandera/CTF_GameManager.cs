a8d7efc0baf2215d75e4bd6743bbbbc32b18b678
CalamityFoxy <134015131+CalamityFoxy@users.noreply.github.com>
2026-06-16 14:58:39 -0300

    algunos cambios uwu

--- Assets/_Scripts/Bandera/CTF_GameManager.cs
+++ Assets/_Scripts/Bandera/CTF_GameManager.cs
@@ -1,27 +1,61 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CTF_GameManager : MonoBehaviour
 {
     public static CTF_GameManager Instance;
 
+    [Header("Score")]
     private int teamAScore;
     private int teamBScore;
-
     public TextMeshProUGUI playerTeamText;
     public TextMeshProUGUI iaTeamText;
 
+    [Header("Condiciones de fin")]
+    [SerializeField] private int scoreToWin = 5;        
+    [SerializeField] private float timeLimit = 120f;    
+    private float timer;
+    private bool gameEnded = false;
+
+    [Header("UI de fin")]
+    [SerializeField] private GameObject victoryPanel;   
+    [SerializeField] private GameObject defeatPanel;    
+    public TextMeshProUGUI timerText;                   
 
     private void Awake()
     {
         Instance = this;
-
         teamAScore = 0;
         teamBScore = 0;
+        timer = timeLimit;
+    }
+
+    private void Update()
+    {
+        if (gameEnded) return;
+
+        
+        timer -= Time.deltaTime;
+
+        
+        if (timerText != null)
+        {
+            int seconds = Mathf.CeilToInt(timer);
+            timerText.text = seconds.ToString();
+        }
+
+        
+        if (timer <= 0f)
+        {
+            Defeat();
+        }
     }
 
     public void AddScore(Team team)
     {
+        if (gameEnded) return;   
+
         if (team == Team.Player)
             teamAScore++;
         else
@@ -29,5 +63,33 @@ public class CTF_GameManager : MonoBehaviour
 
         playerTeamText.text = teamAScore.ToString();
         iaTeamText.text = teamBScore.ToString();
+
+        
+        if (teamAScore >= scoreToWin)
+        {
+            Victory();
+        }
+    }
+
+    private void Victory()
+    {
+        gameEnded = true;
+        
+        if (victoryPanel != null) victoryPanel.SetActive(true);
+        Time.timeScale = 0f;   
+    }
+
+    private void Defeat()
+    {
+        gameEnded = true;
+        
+        if (defeatPanel != null) defeatPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;   
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
